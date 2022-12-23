f87d08989a1905e5db1645160e069d8ec30c57a6
hjeudc <truongminhhieu152@gmail.com>
2022-12-23 13:45:45 +0700

    step 13

---
+++ lib/service/auth/auth_exceptions.dart
@@ -0,0 +1,18 @@
+
+
+//login
+class UserNotFoundAuthException implements Exception {}
+
+class WrongPasswordAuthException implements Exception {}
+
+//register
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlreadyInUseAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
+
+//generic
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
