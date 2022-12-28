936269d70e3ccac17d068857e0e9cb8e5476c31a
hjeudc <truongminhhieu152@gmail.com>
2022-12-28 15:22:58 +0700

    step 19

---
+++ lib/service/cloud/could_note.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:mynotes/service/cloud/cloud_storage_constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+@immutable
+class CloudNote {
+  final String documentId;
+  final String ownerUserId;
+  final String text;
+
+  const CloudNote({
+    required this.documentId,
+    required this.ownerUserId,
+    required this.text,
+  });
+
+  CloudNote.fromSnapshot(QueryDocumentSnapshot<Map<String, dynamic>> snapshot)
+    : documentId = snapshot.id,
+      ownerUserId = snapshot.data()[ownerUserIdFieldName],
+      text = snapshot.data()[textFieldName] as String;
+}
